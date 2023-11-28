7c22c046c8c6107764930791b27d9f793c379604
Zubair Islam Epty <33396274+zubEpty@users.noreply.github.com>
2023-11-28 16:37:59 +0600

    Code and package refactored

---
+++ Assets/Packages/com.studio23.ss2.authsystem/Runtime/Data/ProviderBase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Studio23.SS2.AuthSystem.Data
+{
+    public abstract class ProviderBase : ScriptableObject
+    {
+        public abstract void Authenticate();
+    }
+
+}
