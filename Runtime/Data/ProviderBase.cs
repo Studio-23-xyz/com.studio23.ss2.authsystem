d2dae5783996d83e9caef58905acd19988206584
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-19 23:43:24 +0600

    Merge pull request #4 from Studio-23-xyz/dev
    2.0.6

---
+++ Runtime/Data/ProviderBase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Studio23.SS2.AuthSystem.Data
+{
+    public abstract class ProviderBase : ScriptableObject
+    {
+        public UnityEvent OnAuthSuccess;
+        public abstract void Authenticate();
+        public abstract UserData GetUserData();
+    }
+
+}
