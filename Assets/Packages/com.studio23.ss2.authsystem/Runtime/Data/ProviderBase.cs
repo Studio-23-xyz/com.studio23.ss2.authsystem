7c27d7f787e4f5fc01c5c1ac0db24cfa91fe05a6
Zubair Islam Epty <eptyt4@gmail.com>
2024-01-04 15:24:03 +0600

    2.0.9
    - updated providerbase class

--- Assets/Packages/com.studio23.ss2.authsystem/Runtime/Data/ProviderBase.cs
+++ Assets/Packages/com.studio23.ss2.authsystem/Runtime/Data/ProviderBase.cs
@@ -3,7 +3,7 @@ using UnityEngine.Events;
 
 namespace Studio23.SS2.AuthSystem.Data
 {
-    public abstract class ProviderBase : ScriptableObject
+    public abstract class ProviderBase : MonoBehaviour
     {
         public UnityEvent OnAuthSuccess;
         public UnityEvent OnAuthFailed;
